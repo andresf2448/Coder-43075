dce8b10e2860751e721c317f7fd3bf53ca04ccbd
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-05-11 09:18:51 -0500

    After 2

--- After 2/main.js
+++ After 2/main.js
@@ -52,8 +52,41 @@
 //   return x + y;
 // }
 
-const sumar = (x, y) => x + y;
-const saludar = (nombre) => `Hola ${nombre}`;
+// const sumar = (x, y) => x + y;
+// const saludar = (nombre) => `Hola ${nombre}`;
 
-console.log(sumar(4, 5));
-console.log(saludar("andres"));
+// console.log(sumar(4, 5));
+// console.log(saludar("andres"));
+
+// Array[(turno1, turno2, turno3)];
+// class Turno {
+//   constructor(nombre, direccion, pax, servicio) {
+//     this.nombre = nombre;
+//     this.direccion = direccion;
+//     this.pax = pax;
+//     this.servicio = servicio;
+//   }
+// }
+
+// let eventosAgendados = [];
+// const turno1 = new Turno("Ana", "1ro de mayo 1467", 36, "castillo");
+// const turno2 = new Turno("Laura", "Juan b justo 255", 38, "Titeres");
+// const turno3 = new Turno("Jesica", "Sarmiento 255", 45, "Animacion");
+
+// eventosAgendados.push(turno1, turno2, turno3);
+// console.log(eventosAgendados);
+
+// for (let butaca = 1; butaca <= 5; butaca++) {
+//   let edad = Number(prompt("Ingrese du edad"));
+//   let nombre = prompt("Ingrese su nombre");
+//   let mensaje = `Hola ${nombre}`;
+//   alert(mensaje);
+
+//   if (edad >= 18) {
+//     let dni = Number(prompt("Ingrese su DNI"));
+//     alert(`Butaca #${butaca} DNI ${dni}`);
+//   } else {
+//     alert("Debe ser mayor de edad");
+//   }
+// }
+// alert("Entradas Agotadas");
