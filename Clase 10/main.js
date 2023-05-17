6efeaaf20760bd9b5d8cda21d9b371d7988d1386
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-05-17 09:40:15 -0500

    Clase 10

--- Clase 10/main.js
+++ Clase 10/main.js
@@ -18,3 +18,74 @@ let boton = document.getElementById("boton");
 // };
 
 // boton.addEventListener("click", () => respuesta("julio"));
+
+//EVENTOS MOUSE
+// boton.addEventListener("mousedown", () => console.log("mousedown"));
+// boton.addEventListener("mouseup", () => console.log("mouseup"));
+// boton.addEventListener("mouseover", () => console.log("mouseover"));
+// boton.addEventListener("mouseout", () => console.log("mouseout"));
+// boton.addEventListener("mousemove", () => console.log("mousemove"));
+// boton.addEventListener("click", () => console.log("click"));
+// boton.addEventListener("dblclick", () => console.log("dblclick"));
+
+// boton.addEventListener("mousedown", () => console.log("mousedown"));
+// boton.addEventListener("mouseup", () => console.log("mouseup"));
+// boton.addEventListener("click", () => console.log("click"));
+
+// let saludo = document.getElementById("saludo");
+
+// boton.addEventListener("mousedown", () => {
+//   saludo.className = "verde";
+// });
+
+// boton.addEventListener("mouseover", () => {
+//   saludo.className = "rojo";
+// });
+
+// boton.addEventListener("mouseout", () => {
+//   saludo.className = "amarillo";
+// });
+
+// boton.addEventListener("click", () => {
+//   saludo.className = "azul";
+// });
+
+//EVENTOS TECLADO
+let nombre = document.getElementById("nombre");
+let saludo = document.getElementById("saludo");
+// nombre.addEventListener("keydown", () => console.log("keydown"));
+// nombre.addEventListener("keyup", () => console.log("keyup"));
+
+// nombre.addEventListener("keyup", (e) => {
+//   if (e.key === "a") {
+//     saludo.className = "verde";
+//   } else if (e.key === "s") {
+//     saludo.className = "rojo";
+//   } else if (e.key === "d") {
+//     saludo.className = "amarillo";
+//   } else {
+//     saludo.className = "azul";
+//   }
+// });
+
+// nombre.addEventListener("input", () => {
+//   if(!nombre.value.includes("@")){
+//     saludo.className = "rojo";
+//   }else{
+//     saludo.className = "blanco";
+//   }
+// });
+
+let formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  
+  let inputs = e.target.children;
+  // console.log(inputs[0].value);
+  // console.log(inputs[1].value);
+
+  if(!inputs[0].value.includes("@")){
+    inputs[0].value = "";
+  }
+});
